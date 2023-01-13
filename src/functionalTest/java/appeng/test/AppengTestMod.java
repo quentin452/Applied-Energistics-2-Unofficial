95ac6314aa27224e25206182b6f789925d1dfb37
Raven Szewczyk <git@eigenraven.me>
2023-01-13 19:16:55 +0000

    Integrate JUnit 5 into functionalTests

--- src/functionalTest/java/appeng/test/AppengTestMod.java
+++ src/functionalTest/java/appeng/test/AppengTestMod.java
@@ -1,10 +1,22 @@
 package appeng.test;
 
+import cpw.mods.fml.common.FMLCommonHandler;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLServerStartedEvent;
+import java.io.PrintWriter;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.ChatComponentText;
+import org.apache.commons.io.output.CloseShieldOutputStream;
+import org.junit.platform.engine.discovery.DiscoverySelectors;
+import org.junit.platform.launcher.Launcher;
+import org.junit.platform.launcher.LauncherDiscoveryRequest;
+import org.junit.platform.launcher.LauncherSession;
+import org.junit.platform.launcher.TestPlan;
+import org.junit.platform.launcher.core.LauncherDiscoveryRequestBuilder;
+import org.junit.platform.launcher.core.LauncherFactory;
+import org.junit.platform.launcher.listeners.SummaryGeneratingListener;
+import org.junit.platform.launcher.listeners.TestExecutionSummary;
 
 // Most of these don't matter as this mod never gets published
 @Mod(
@@ -21,6 +33,27 @@ public class AppengTestMod {
     }
 
     public void runTests() {
-        //
+        // https://junit.org/junit5/docs/current/user-guide/#launcher-api
+        final LauncherDiscoveryRequest discovery = LauncherDiscoveryRequestBuilder.request()
+                .selectors(DiscoverySelectors.selectPackage("appeng.test"))
+                .build();
+        final SummaryGeneratingListener listener = new SummaryGeneratingListener();
+        try (LauncherSession session = LauncherFactory.openSession()) {
+            final Launcher launcher = session.getLauncher();
+            final TestPlan plan = launcher.discover(discovery);
+            launcher.registerTestExecutionListeners(listener);
+            launcher.execute(plan);
+        }
+        TestExecutionSummary summary = listener.getSummary();
+        try (PrintWriter stderrWriter = new PrintWriter(new CloseShieldOutputStream(System.err), true)) {
+            summary.printFailuresTo(stderrWriter, 32);
+            summary.printTo(stderrWriter);
+            stderrWriter.flush();
+        }
+        // Throw an exception if running via `runServer`
+        if (summary.getTotalFailureCount() > 0
+                && FMLCommonHandler.instance().getSide().isServer()) {
+            throw new RuntimeException("Some of the unit tests failed to execute, check the log for details");
+        }
     }
 }
---
+++ src/functionalTest/java/appeng/test/CraftingV2Tests.java
@@ -0,0 +1,8 @@
+package appeng.test;
+
+import org.junit.jupiter.api.Test;
+
+public class CraftingV2Tests {
+    @Test
+    void helloWorld() {}
+}
