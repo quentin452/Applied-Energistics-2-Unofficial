8cb271f44bacb8b4f624c1dc35c07fd697c77cae
Firenoo <49818773+firenoo@users.noreply.github.com>
2023-03-26 16:30:15 -0700

    Optimize how the ME terminals are updated.
    Use a tree instead of clearing, refiltering, and resorting every time. Adds
    cached fields to determine whether the terminal needs to be completely
    resorted, such as when the sort order or sort direction is changed.
    
    Implementation note - Java TreeSet implementation considers two elements to
    be equal if Comparator#compare(o1, o2) == 0. Thus, we need to be careful
    when implementing the comparators.

---
+++ src/main/java/appeng/util/IItemTree.java
@@ -0,0 +1,52 @@
+package appeng.util;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.Predicate;
+
+import net.minecraft.item.ItemStack;
+
+import appeng.api.storage.data.IAEItemStack;
+
+public interface IItemTree extends Iterable<IAEItemStack> {
+
+    /**
+     * ItemStack display list of the items.
+     */
+    List<ItemStack> displayList();
+
+    /**
+     * IAEItemStack view list of the items.
+     */
+    List<IAEItemStack> viewList();
+
+    /**
+     * Resort the entire list using the new comparator and filter.
+     */
+    void refresh(Comparator<IAEItemStack> comparator, Predicate<IAEItemStack> filter);
+
+    /**
+     * Prune the sorted list using the new filter.
+     */
+    void refresh(Predicate<IAEItemStack> filter);
+
+    /**
+     * Resort the entire list using the new comparator with the cached filter.
+     */
+    void refresh(Comparator<IAEItemStack> comparator);
+
+    /**
+     * Refresh the view/display lists. No resorting is done.
+     */
+    void refresh();
+
+    /**
+     * Update the list with the stack. This does not refresh the list until updateView() is called.
+     */
+    void update(IAEItemStack stack);
+
+    /**
+     * Remove all elements from the backing list. Triggers a refresh.
+     */
+    void clear();
+}
